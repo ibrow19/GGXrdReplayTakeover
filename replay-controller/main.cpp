f9ae866d7d6db79d27007361220301d7a42787f4
Innes Brown <innes.brown19@gmail.com>
2025-06-15 00:03:52 +0100

    Basic dll for testing injecting

---
+++ replay-controller/main.cpp
@@ -0,0 +1,31 @@
+#include <windows.h>
+#include <imgui.h>
+#include <imgui_impl_dx9.h>
+#include <imgui_impl_win32.h>
+#include <d3d9.h>
+
+// TODO: link via cmake
+#pragma comment (lib, "user32.lib")
+
+void attach()
+{
+    MessageBox(NULL, "DLL successfully loaded", "Replay Controller", MB_OK);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
+{
+    switch (reason)
+    {
+        case DLL_PROCESS_ATTACH:
+            attach();
+            break;
+        case DLL_PROCESS_DETACH:
+            break;
+        case DLL_THREAD_ATTACH:
+            break;
+        case DLL_THREAD_DETACH:
+            break;
+    }
+
+    return TRUE;
+}
