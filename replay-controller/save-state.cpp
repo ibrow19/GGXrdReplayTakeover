7b0b0f6e9694332253465901bdee2b679c05df10
Innes Brown <innes.brown19@gmail.com>
2026-04-03 04:19:39 +0100

    Remove TODO now that it is clear that the camera pointer that needs cleared on reset is just a pointer to the start of the list we are already manually destroying

--- replay-controller/save-state.cpp
+++ replay-controller/save-state.cpp
@@ -259,17 +259,16 @@ static void ResetGameState()
     resetGameUe(manager.GetPtr());
 
     // Clean up camera aim state. The game's reset functions don't fully
-    // clean up the list of active camera animations and another actor
-    // at BattleCamera+0x510 that needs to be destroyed. Normally these
-    // will be cleaned up next tick but if we are immediately loading
-    // another state that has active camera anims then we need to manually
+    // clean up the list of active camera animations and a pointer to 
+    // one of those animations at BattleCamera+0x510. The above reset 
+    // flags these objects for destruction and normally they will be 
+    // cleaned up on the next tick. But if we are immediately loading 
+    // another state that has active camera anims then we need to manually 
     // destroy these anims to prevent them being re-used by the loaded state.
     DWORD battleCamera = manager.GetBattleCamera();
     RemoveCameraAnimFunc removeAnim = XrdModule::GetRemoveCameraAnim();
-
-    // TODO: this will leak atm as actor is not properly destroyed.
-    DWORD& cameraTransformThing = *(DWORD*)(battleCamera + 0x510);
-    cameraTransformThing = 0;
+    DWORD& firstAnimPointer = *(DWORD*)(battleCamera + 0x510);
+    firstAnimPointer = 0;
 
     // Iterate active anims and remove them. There is probably a reset array 
     // function somewhere we can use but we will remove them one at a time the 
